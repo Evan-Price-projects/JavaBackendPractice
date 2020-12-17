b319fcb7e98687674cd73809e28da472bf3213ae
Evan <eprice1223321@gmail.com>
2020-12-17 11:14:16 -0600

    ADD: higher lower game complete

--- javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
+++ javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
@@ -2,8 +2,11 @@ package com.example.javaBackendPractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class JavaBackendPracticeApplication {
@@ -21,9 +24,46 @@ public class JavaBackendPracticeApplication {
 			this.PlayerNumber = PlayerNumber;
 			this.PlayerNames = PlayerNames;
 		}
-	public static class GameLobby{
-
+	public static class Game{
+		private static Integer Guesses;
+		public void setGuesses(Integer guess){
+			this.Guesses = guess;
+		}
+		public Integer getGuesses(){return Guesses;}
 	}
+	}
+	public static void Game(){
+		System.out.println("Guess a number between 1 and ?");
+		Integer x = -100; Integer z ;
+		Scanner in = new Scanner(System.in);
+		String y = in.nextLine();
+		Random randomGenerator = new Random();
+		x = Integer.parseInt(y);
+		if (x == null){return;}
+		else{
+			z = randomGenerator.nextInt(x);
+		}
+		x = -100;
+		for(int i = 0; i< 5; i++) {
+			try {
+				y = in.nextLine();
+				x = Integer.parseInt(y);
+				if (x == null | x == 0) {
+					return;
+				} else if (x == z) {
+					System.out.println("WINNER");
+					return;
+				}
+				else if ( x > z){
+					System.out.println("Lower");
+				}
+				else {
+					System.out.println("Higher");
+				}
+			} catch (Exception e) {
+				System.out.print("Exception");
+			}
+		}
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBackendPracticeApplication.class, args);
@@ -37,5 +77,6 @@ public class JavaBackendPracticeApplication {
 		list.add("Eli");
 		CreateGame game = new CreateGame (5, list);
 		System.out.println(game.PlayerNames);
-}
+		Game();
+	}
 	}
