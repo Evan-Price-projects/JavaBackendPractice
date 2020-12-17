e163c3704fee00203fc2fb9ea63550172e7f6c29
Evan <eprice1223321@gmail.com>
2020-12-17 16:00:49 -0600

    ADD: Got user information where I want it now implementing either db or password. Probably db

--- javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
+++ javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
@@ -2,10 +2,11 @@ package com.example.javaBackendPractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.print.attribute.standard.Chromaticity;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Hashtable;
 
 @SpringBootApplication
 public class JavaBackendPracticeApplication {
@@ -13,20 +14,53 @@ public class JavaBackendPracticeApplication {
 		private static String Username;
 		private static String Address;
 		private static String Birthdate;
+		private static String City;
+		private static String State;
+		private static String Zip;
+		private static Hashtable<String, ArrayList> Collection;
 
 
-		public User(String Username) {
+
+		public User(String Username, String Address, String Birthdate, String City, String State, String Zip) {
 			this.Username = Username;
+			this.Address = Address;
+			this.Birthdate = Birthdate;
+			this.City = City;
+			this.State = State;
+			this.Zip = Zip;
+			this.Collection = new Hashtable<String,ArrayList>();
+			ArrayList<String> combo = new ArrayList<String>();
+			combo.add(this.Username);
+			combo.add(this.Address);
+			combo.add(this.Birthdate);
+			combo.add(this.City);
+			combo.add(this.State);
+			combo.add(this.Zip);
+			this.Collection.put(this.Username,combo);
 		}
 	}
+//********************************************************************
+//	Game: Created by Evan Price
+//  Inputs: User
+//  Outputs: void
+//	Goal: choose a number that is the same as the random number generated
+//********************************************************************
 
 	public static class Game{
 		private static Integer Guesses;
 		private static Integer Solution;
-		public void setGuesses(Integer guess){
-			this.Guesses = guess;
+
+		public void setGuesses(Integer guess)
+		{ if (guess != null){
+			this.Guesses = guess;}
 		}
 		public Integer getGuesses(){return Guesses;}
+
+		public void setSolution(Integer solution){
+			Random rand = new Random();
+			this.Solution = rand.nextInt(solution);
+		}
+		public Integer getSolution(){return Solution;}
 	}
 	public static void Game(){
 		Game newGame = new Game();
@@ -34,46 +68,39 @@ public class JavaBackendPracticeApplication {
 
 		System.out.println("Guess a number between 1 and ?");
 		Scanner in = new Scanner(System.in);
-		String y = in.nextLine();
-		Random randomGenerator = new Random();
-
-		newGame.setGuesses(Integer.parseInt(y));
+		String guess = in.nextLine();
+		newGame.setGuesses(Integer.parseInt(guess));
 		if (newGame.Guesses == null){return;}
 		else{
-			z = randomGenerator.nextInt(x);
+			newGame.setSolution(newGame.Guesses);
 		}
-		x = -100;
 		for(int i = 0; i< 5; i++) {
 			try {
-				y = in.nextLine();
-				x = Integer.parseInt(y);
-				if (x == null | x == 0) {
+				guess = in.nextLine();
+				newGame.setGuesses(Integer.parseInt(guess));
+				if (newGame.getGuesses() == null | newGame.getGuesses() == 0) {
 					return;
-				} else if (x == z) {
+				} else if (newGame.getGuesses()== newGame.getSolution()) {
 					System.out.println("WINNER");
 					return;
 				}
-				else if ( x > z){
+				else if ( newGame.getGuesses()  > newGame.getSolution()){
 					System.out.println("Lower");
 				}
 				else {
 					System.out.println("Higher");
 				}
 			} catch (Exception e) {
-				System.out.print("Exception");
+				System.out.print(e);
 			}
 		}
+		System.out.println("The Solution was:" + newGame.getSolution());
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBackendPracticeApplication.class, args);
-		User user = new User("Bob");
-		System.out.println(user.Username);
-		List<String> list = new ArrayList<String>();
-		list.add("Evan");
-		list.add("Andrew");
-		list.add("Jacob");
-		list.add("Rachel");
-		list.add("Eli");
-		Game();
+		User user = new User("Bob","123 Fake St", "1/11/11","Iowa City","Iowa", "52245");
+		System.out.println(user.Collection);
+		//Game();
 	}
 	}
