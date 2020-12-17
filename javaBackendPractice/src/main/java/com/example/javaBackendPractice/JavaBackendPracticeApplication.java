8a01617ee78ac18555c0fea57042191691aedfe3
Evan <eprice1223321@gmail.com>
2020-12-17 10:43:41 -0600

    ADD: added second class, now working on making it a game. Changed it from create user to just user. I will create one at a later time

--- javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
+++ javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
@@ -2,21 +2,40 @@ package com.example.javaBackendPractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class JavaBackendPracticeApplication {
-	public static class CreateUser {
-		public static String Username;
-		public static String Password;
+	public static class User {
+		private static String Username;
 
-		public CreateUser(String Username, String Password) {
+		public User(String Username) {
 			this.Username = Username;
-			this.Password = Password;
 		}
+	}
+	public static class CreateGame {
+		private static Integer PlayerNumber;
+		private static List PlayerNames;
+		public CreateGame (Integer PlayerNumber, List PlayerNames) {
+			this.PlayerNumber = PlayerNumber;
+			this.PlayerNames = PlayerNames;
+		}
+	public static class GameLobby{
+
+	}
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBackendPracticeApplication.class, args);
-		CreateUser user = new CreateUser("Bob","Password");
+		User user = new User("Bob");
 		System.out.println(user.Username);
+		List<String> list = new ArrayList<String>();
+		list.add("Evan");
+		list.add("Andrew");
+		list.add("Jacob");
+		list.add("Rachel");
+		list.add("Eli");
+		CreateGame game = new CreateGame (5, list);
+		System.out.println(game.PlayerNames);
 }
 	}
