7c0da7ca236746c3eec146553a611d5be339d155
Evan <eprice1223321@gmail.com>
2020-12-17 10:30:47 -0600

    ADD: got initial username functionality set up now time to do some interesting stuff.

--- javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
+++ javaBackendPractice/src/main/java/com/example/javaBackendPractice/JavaBackendPracticeApplication.java
@@ -1,13 +1,22 @@
 package com.example.javaBackendPractice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JavaBackendPracticeApplication {
+	public static class CreateUser {
+		public static String Username;
+		public static String Password;
 
+		public CreateUser(String Username, String Password) {
+			this.Username = Username;
+			this.Password = Password;
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBackendPracticeApplication.class, args);
-	}
-
+		CreateUser user = new CreateUser("Bob","Password");
+		System.out.println(user.Username);
 }
+	}
